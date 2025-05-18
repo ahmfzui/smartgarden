793ab41d11f30f712290f4ad6249e015d99951ba
ahmfzui <ahmadfaauzi2304@gmail.com>
2025-05-18 19:01:50 +0700

    waww

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // This allows production builds to complete even with ESLint errors
     ignoreDuringBuilds: true,
   },
-  /* other config options you might have */
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
 };
 
 export default nextConfig;
